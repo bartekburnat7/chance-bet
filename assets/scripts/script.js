65bd3535f03e446fe380d4dfbddbaebeed7b8436
bartekburnat7 <>
2023-06-19 20:31:09 +0000

    add circle and spin animation to the game section. Added a script that reads the input and then checks if it is even or odd.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,26 @@
 var balance = 200;
 
-const gameButton = document.getElementById("place_bet");
+const gameButton = document.getElementById("place_bet_roulette");
 gameButton.addEventListener("click", place_bet_roulette);
 
-function place_bet_place_bet_roulette() {
+function place_bet_roulette() {
     var place_bet_roulette = document.getElementById("number_roulette");
-    generateNumber();
+    const current_number = generate_number();
+    console.log(current_number);
+    even_odd(current_number);
 }
+
 
 function generate_number() {
     return Math.floor(Math.random() * 15);
 }
 
-var random_number = generateNumber();
+function even_odd(x) {
+    if (x === 0) {
+        return 0;
+    } else if ((x & 1) === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
