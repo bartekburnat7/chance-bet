e43375873c02503948e78c6bc439a240c4bed8aa
bartekburnat7 <>
2023-06-21 18:58:04 +0000

    Created a logic for each spin.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,20 +7,30 @@ function place_bet_roulette() {
     var place_bet_roulette = document.getElementById("number_roulette");
     const current_number = generate_number();
     console.log(current_number);
-    even_odd(current_number);
+    even_odd_verify(current_number);
 }
 
 
 function generate_number() {
-    return Math.floor(Math.random() * 15);
+    return Math.floor(Math.random() * 12);
 }
 
-function even_odd(x) {
-    if (x === 0) {
-        return 0;
-    } else if ((x & 1) === 0) {
-        console.log("even");
-    } else {
-        console.log("odd");
+function even_odd_verify(x) {
+    if (x >= 0 && x < 4) {
+        var element = document.getElementById("mainbox");
+        element.classList.add("spin_green");
+        return 1;
+    } else if (x >= 4 && x < 7) {
+        var element = document.getElementById("mainbox");
+        element.classList.add("spin_blue");
+        return 2;
+    } else if (x >= 7 && x < 10) {
+        var element = document.getElementById("mainbox");
+        element.classList.add("spin_orange");
+        return 3;
+    }else if (x >= 10){
+        var element = document.getElementById("mainbox");
+        element.classList.add("spin_white");
+        return 4;
     }
 }
